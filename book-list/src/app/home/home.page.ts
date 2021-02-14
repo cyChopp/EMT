aada19e0cb5ebb1d8ec27a0073b28ca134389a8b
cyChopp <45396059+cyChopp@users.noreply.github.com>
2021-02-14 13:33:07 +0100

    Add ionic routing

---
+++ book-list/src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+text = 'Text'
+  constructor() {}
+onChangeText(){
+  this.text='This text was changed'
+}
+}
